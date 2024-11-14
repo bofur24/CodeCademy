835f63e0e560a81916c4c4f71ddeb106eae1d5aa
Keith Heffington <lightingcount_@hotmail.com>
2024-11-14 15:41:12 -0600

    Started Numbers and operators

---
+++ C#/Numbers-Operators/NumbersOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Number of pizza shops
+int pizzaShops = 4332;
+
+// Number of employees
+int totalEmployees = 86928;
+
+// Revenue
+decimal revenue = 390819.28m;
+
+// Log the values to the console:
+Console.WriteLine($"Total pizza shops: {pizzaShops}.");
+Console.WriteLine($"Total Employees: {totalEmployees}.");
+Console.WriteLine($"Total revenue: {revenue}.");
