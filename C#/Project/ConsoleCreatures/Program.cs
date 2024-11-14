bcef6e641bbe12ef8a8404d41dcb73a14f7b4b8f
Keith Heffington <lightingcount_@hotmail.com>
2024-11-14 09:14:21 -0600

    Project Moved around and setup

---
+++ C#/Project/ConsoleCreatures/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Working with Console.Writeline to print out a Creature
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+Console.WriteLine("Booly!!");
+
