ae4ee8f55a5c44394148a9e445e3c92fc6e85da2
Keith Heffington <lightingcount_@hotmail.com>
2024-11-14 09:35:19 -0600

    Creating Variables with Types

--- C#/DataTypes-Variables/DataTypes/Program.cs
+++ C#/DataTypes-Variables/DataTypes/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// Creating Variables with Types
+// Create Variables
+string name = "Shadow";
+string breed = "Golden Retriever";
+int age = 5;
+double weight = 65.22;
+bool spayed = true;
+
+// Print variables to the console
+Console.WriteLine($"Your dogs name is {name}.");
+Console.WriteLine($"Your dogs breed is {breed}.");
+Console.WriteLine($"Your dogs age is {age}.");
+Console.WriteLine($"Your dogs weight is {weight}.");
+Console.WriteLine($"Your dog is spayed {spayed}.");
