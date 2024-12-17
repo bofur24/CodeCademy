2cef8c1030af2c1686a6f28a3b04556328a22301
Keith Heffington <keith.heffington@gmail.com>
2024-12-17 06:58:54 -0600

    Started Boolean Data Types

---
+++ C#/Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Boolean Data Types
+
+Console.Write("The number 500 is greater than 24? (True or False)");
+bool answerOne = Convert.ToBoolean(Console.ReadLine());
+if (answerOne == true) {
+    Console.WriteLine($"Correct{answerOne}, 500 is greater than 24.");
+}else
+{
+    Console.WriteLine($"{answerOne}, 24 is not greater than 500");
+}
+
