fa9826de4990376af6c757b0f65229a2e5d6f4dc
Keith Heffington <lightingcount_@hotmail.com>
2024-12-19 16:48:19 -0600

    Started work on new project

---
+++ C#/Project/SpaceExpedition/Program.cs
@@ -0,0 +1,14 @@
+﻿// Space expedition Boolean Adventure
+
+Console.WriteLine("You're the captain of a space expedition aimed at discovering new planets.");
+Console.WriteLine("Your space agency has established a set of parameters that need regular evaluations to make informed decisions.");
+Console.WriteLine("These parameters include atmospheric quality, gravity levels, and resource availability.")
+
+Console.WriteLine("You have discovered 3 planets in the area");
+Console.WriteLine("Argos Rho, Armstrong Nebula, and Artemis Tau");
+Console.Write("What planet would you like to scan? ");
+string planetScan = Console.ReadLine();
+
+if (planetScan == "Argos Rho") {
+  bool argos
+}
